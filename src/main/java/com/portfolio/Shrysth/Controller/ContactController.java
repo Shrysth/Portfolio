28d74e9578adbac8a504b25384e75d6ff121eed9
Shrysth <shrysthshukla@yahoo.com>
2025-05-03 17:49:00 +0530

    revert mail

--- src/main/java/com/portfolio/Shrysth/Controller/ContactController.java
+++ src/main/java/com/portfolio/Shrysth/Controller/ContactController.java
@@ -26,6 +26,18 @@ public ResponseEntity<String> sendContactEmail(@ModelAttribute ContactForm conta
         // Call updated service method with reply-to address
         emailService.sendEmail("shrysthshukla@yahoo.com", subject, body, contactForm.getEmail());
 
+        String subject2 = "Thank you For Contacting";
+        String body2 = "Hi "+contactForm.getName() +",\n" + //
+                        "\n" + //
+                        "Thank you for contacting me through my portfolio website — http://portfolio-production-d593.up.railway.app/. I appreciate you taking the time to get in touch.\n" + //
+                        "\n" + //
+                        "I’ve received your message and will get back to you as soon as possible. In the meantime, feel free to explore more of my work or connect with me on LinkedIn or GitHub.\n" + //
+                        "\n" + //
+                        "Looking forward to speaking with you soon!\n" + //
+                        "\n" + //
+                        "Best regards,\n" + //
+                        "Shrysth Shukla";
+        emailService.sendEmail(contactForm.getEmail(), subject2, body2, "siddshrysth@gmail.com");
         return ResponseEntity.ok("email sent");
     } catch (Exception e) {
         return ResponseEntity.status(500).body(e.getMessage());
