3827690c7d4b38e0da47dabd91533bc4ccddaf9d
Shrysth <shrysthshukla@yahoo.com>
2025-05-10 23:01:25 +0530

    speed update

--- src/main/java/com/portfolio/Shrysth/Service/EmailService.java
+++ src/main/java/com/portfolio/Shrysth/Service/EmailService.java
@@ -3,6 +3,7 @@ package com.portfolio.Shrysth.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String body, String replyTo) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("siddshrysth@gmail.com");
--- src/main/java/com/portfolio/Shrysth/ShrysthApplication.java
+++ src/main/java/com/portfolio/Shrysth/ShrysthApplication.java
@@ -2,8 +2,10 @@ package com.portfolio.Shrysth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ShrysthApplication {
 
 	public static void main(String[] args) {
